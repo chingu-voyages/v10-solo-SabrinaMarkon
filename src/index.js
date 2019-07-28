7a3db9b2d106e28bf3f9f97872b742153a1420b6
Sabrina Markon <phpsitescripts@outlook.com>
2019-07-27 19:55:43 -0600

    Move containers out of components folder.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/containers/App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
