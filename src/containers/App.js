2fb552c0eb611c7f2e1d07d49a22fdff466dbcfc
Sabrina Markon <phpsitescripts@outlook.com>
2019-07-28 12:34:52 -0600

    Move nav into container div and fix Home route.

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,55 +20,55 @@ export default class App extends Component {
       <Router>
         <div className="container">
           <h1>Hire a Team!</h1>
+          {/* navigation  */}
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  {/* Do not include a Route with the App component or there will be an infinite loop because this is ALREADY the App component!  */}
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/howitworks" className="nav-link">How It Works!</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/signup" className="nav-link">Signup</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">Login</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/postjob" className="nav-link">Post a Job!</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          {/* ROUTES  */}
+          {/* 1. not logged in  */}
+          <Route path="/howitworks" component={HowItWorks} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/postjob" component={PostJob} />
+          <Route path="/contact" component={Contact} />
+    
+          {/* 2. logged in  */}
+          {/* main members area after login - maybe reputation here? */}
+          <Route path="/main" component={UserMain} />
+          {/* profile should include reputation information (maybe) */}
+          <Route path="/profile" component={Profile} />
+          {/* teams you are a member of, create a new team, invites, invite others */}
+          <Route path="/teams" component={Teams} />
+          {/* jobs your team is working on. jobs you want to hire a team for. job list to bid on. */}
+          <Route path="/jobs/:type" component={Jobs} />
+          {/* your earnings, your teams' earnings per team, owed and by which employer. Payments you owe others you have hired  */}
+          <Route path="/money/:type" component={Money} />
+    
+          {/* custom 404  */}
+          <Route component={NotFound} />
         </div>
-        {/* navigation  */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse nav-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">Home</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/howitworks" className="nav-link">How It Works!</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/signup" className="nav-link">Signup</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/login" className="nav-link">Login</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/postjob" className="nav-link">Post a Job!</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/contact" className="nav-link">Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      
-        {/* not logged in  */}
-        {/* <Route exact path="/" component={App} /> */}
-        <Route path="/howitworks" component={HowItWorks} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/postjob" component={PostJob} />
-        <Route path="/contact" component={Contact} />
-  
-        {/* logged in  */}
-        {/* main members area after login - maybe reputation here? */}
-        <Route path="/main" component={UserMain} />
-        {/* profile should include reputation information (maybe) */}
-        <Route path="/profile" component={Profile} />
-        {/* teams you are a member of, create a new team, invites, invite others */}
-        <Route path="/teams" component={Teams} />
-        {/* jobs your team is working on. jobs you want to hire a team for. job list to bid on. */}
-        <Route path="/jobs/:type" component={Jobs} />
-        {/* your earnings, your teams' earnings per team, owed and by which employer. Payments you owe others you have hired  */}
-        <Route path="/money/:type" component={Money} />
-  
-        {/* custom 404  */}
-        <Route component={NotFound} />
       </Router>
     );
   }
