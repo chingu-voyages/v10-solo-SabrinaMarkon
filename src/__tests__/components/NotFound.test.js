ed02b80608fd7720611ea0714d196351c4de58fc
Sabrina Markon <phpsitescripts@outlook.com>
2019-07-27 23:41:19 -0600

    Create test file for NotFound component.

---
+++ src/__tests__/components/NotFound.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// components
+import NotFound from '../components/NotFound';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<NotFound />);
+});
+
+describe('Testing NotFound component', () => {
+  it('should render an h1 heading with the correct text', () => {
+    expect(wrapper.find('h1').text().toEqual('NotFound'));
+  });
+});
