ffa1cb23ca02bff05a5f70ec825300d191dec38e
Sabrina Markon <phpsitescripts@outlook.com>
2019-07-28 18:49:30 -0600

    Handle multiple select box.

--- src/components/PostJob.js
+++ src/components/PostJob.js
@@ -11,29 +11,39 @@ export default class PostJob extends Component {
       budget: '',
       time_estimate: '',
       categories: [],
-      skill_levels: [],
+      skill_junior: false,
+      skill_intermediate: false,
+      skill_senior: false,
+      skill_lead: false,
       skill_tags: [],
       posted: null,
       completed: null
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   handleChange = event => {
-    this.setState({ 
-      [event.target.id]: event.target.value
-    });
+    if (event.target.id === 'categories') {
+      this.setState({
+        categories: Array.from(event.target.selectedOptions, (item) => item.value)
+      });
+    } else {
+      this.setState({ 
+        [event.target.id]: event.target.value
+      });
+    }
   };
 
   handleSubmit = event => {
     event.preventDefault();
 
     // check.
-    console.log(this.state.title);
+    console.log(this.state.categories);
 
     // validation.
 
-    // send to database.
+    // send to database endpoint.
 
     // reset form.
   }
@@ -45,47 +55,62 @@ export default class PostJob extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="title">Job Title</label>
-            <input type="text" className="form-control" id="title" aria-describedby="Job Title" placeholder="Enter Job Title" value={this.state.title} onChange={this.handleChange} />
+            <input type="text" className="form-control" id="title" aria-describedby="Job Title" 
+            placeholder="Enter Job Title" value={this.state.title} onChange={this.handleChange} />
           </div>
           <div className="form-group">
             <label htmlFor="description">Job Description</label>
-            <textarea className="form-control" id="description" aria-describedby="Job Description" placeholder="Enter Job Description" rows="10" value={this.state.description} onChange={this.handleChange}></textarea>
+            <textarea className="form-control" id="description" aria-describedby="Job Description" 
+            placeholder="Enter Job Description" rows="10" value={this.state.description} onChange={this.handleChange}></textarea>
           </div>
           <div className="form-group">
             <label htmlFor="budget">Job Budget</label>
-            <input type="text" className="form-control" id="budget" aria-describedby="Budget Range" placeholder="Enter Budget Range" value={this.state.budget} onChange={this.handleChange} />
+            <input type="text" className="form-control" id="budget" aria-describedby="Budget Range" 
+            placeholder="Enter Budget Range" value={this.state.budget} onChange={this.handleChange} />
           </div>
           <div className="form-group">
             <label htmlFor="time_estimate">Estimated Time</label>
-            <input type="text" className="form-control" id="time_estimate" aria-describedby="Expected Time" placeholder="Enter Expected Time" value={this.state.time_estimate} onChange={this.handleChange} />
+            <input type="text" className="form-control" id="time_estimate" aria-describedby="Expected Time" 
+            placeholder="Enter Expected Time" value={this.state.time_estimate} onChange={this.handleChange} />
           </div>
           <div className="form-group">
             <div>Select Skill Levels (defaults to all)</div>
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" id="skill_levels1" value="Junior" />
-              <label className="form-check-label" htmlFor="skill_levels1">Junior</label>
+              <input className="form-check-input" type="checkbox" id="skill_junior" value="Junior" 
+              checked={this.state.skill_junior === 'Junior'} onChange={this.handleChange} />
+              <label className="form-check-label" htmlFor="skill_junior">Junior</label>
             </div>
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" id="skill_levels2" value="Intermediate" />
-              <label className="form-check-label" htmlFor="skill_levels2">Intermediate</label>
+              <input className="form-check-input" type="checkbox" id="skill_intermediate" value="Intermediate" 
+              checked={this.state.skill_junior === 'Intermediate'} onChange={this.handleChange} />
+              <label className="form-check-label" htmlFor="skill_intermediate">Intermediate</label>
             </div>
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" id="skill_levels3" value="Senior" />
-              <label className="form-check-label" htmlFor="skill_levels3">Senior</label>
+              <input className="form-check-input" type="checkbox" id="skill_senior" value="Senior" 
+              checked={this.state.skill_junior === 'Senior'} onChange={this.handleChange} />
+              <label className="form-check-label" htmlFor="skill_senior">Senior</label>
             </div>
             <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" id="skill_levels4" value="Lead" />
-              <label className="form-check-label" htmlFor="skill_levels4">Lead</label>
+              <input className="form-check-input" type="checkbox" id="skill_lead" value="Lead" 
+              checked={this.state.skill_junior === 'Lead'} onChange={this.handleChange} />
+              <label className="form-check-label" htmlFor="skill_lead">Lead</label>
             </div>
           </div>
           <div className="form-group">
+            {/* Get categories from database  */}
             <label htmlFor="categories">Categories</label>
-              <select multiple id="categories" className="form-control">
+              <select multiple id="categories" className="form-control" onChange={this.handleChange}>
               <option defaultValue disabled>Choose One or More...</option>
-              <option>Web</option>
-              <option>Game</option>
+              <option>Web Apps</option>
+              <option>Games</option>
               <option>WordPress</option>
-              <option>Other Categories...</option>
+              <option>Ecommerce</option>
+              <option>Mobile</option>
+              <option>Drupal</option>
+              <option>2D Graphics</option>
+              <option>3D Graphics</option>
+              <option>Animation</option>
+              <option>Design</option>
             </select>
           </div>
           <button className="btn btn-primary" type="submit">Submit</button>
