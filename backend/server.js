ba425bf30e5d2ebea1f1d7d3f7424bb78553a17b
Sabrina Markon <phpsitescripts@outlook.com>
2019-08-10 01:19:12 -0600

    Set up MongoDB Atlas connection for backend in server.js. Set up .env for secret values.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
+// get private variables:
+require('dotenv').config({path: '.env'});
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+// connect to mongodb:
+const mongoose = require('mongoose');
 const PORT = 4000;
 
 // pass middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect(process.env.REACT_APP_MONGODB_CONNECTION_STRING, 
+  { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log('MongoDB database connection established successfully!');
+});
 
 app.listen(PORT, function() { 
   console.log('Server is listening on port ' + PORT);
