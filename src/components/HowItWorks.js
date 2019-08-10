2595903f94a7cc57cffd986a69aa8c9ec62f53be
Sabrina Markon <phpsitescripts@outlook.com>
2019-08-09 23:47:16 -0600

    Add lorem ipsum to HowItWorks component.

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -7,8 +7,36 @@ export default class HowItWorks extends Component {
     return (
       <div>
         <h1>HowItWorks</h1>
-        <div>
-          Lorem ipsum
+        <div style={{paddingTop: 20}}>
+
+            <p>I hate yogurt. It's just stuff with bits in.</p>
+            <p>You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better? No, I'll fix it. I'm good at fixing rot. Call me the Rotmeister. No, I'm the Doctor. Don't call me the Rotmeister.</p>
+            <p>I hate yogurt. It's just stuff with bits in. <strong> You've swallowed a planet!</strong> <em> You've swallowed a planet!</em> No… It's a thing; it's like a plan, but with more greatness.</p>
+            <h2>*Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do!</h2>
+            <p>No… It's a thing; it's like a plan, but with more greatness. You've swallowed a planet! I hate yogurt. It's just stuff with bits in. I am the last of my species, and I know how that weighs on the heart so don't lie to me!</p>
+            <ol>
+            <li>You know how I sometimes have really brilliant ideas?</li><li>It's a fez. I wear a fez now. Fezes are cool.</li><li>I am the last of my species, and I know how that weighs on the heart so don't lie to me!</li>
+            </ol>
+
+            <h3>Father Christmas. Santa Claus. Or as I've always known him: Jeff.</h3>
+            <p>It's a fez. I wear a fez now. Fezes are cool. You hate me; you want to kill me! Well, go on! Kill me! KILL ME! *Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do!</p>
+            <ul>
+            <li>The way I see it, every life is a pile of good things and bad things.…hey.…the good things don't always soften the bad things; but vice-versa the bad things don't necessarily spoil the good things and make them unimportant.</li><li>Annihilate? No. No violence. I won't stand for it. Not now, not ever, do you understand me?! I'm the Doctor, the Oncoming Storm - and you basically meant beat them in a football match, didn't you?</li><li>I hate yogurt. It's just stuff with bits in.</li>
+            </ul>
+
+            <p>Saving the world with meals on wheels. No… It's a thing; it's like a plan, but with more greatness. Aw, you're all Mr. Grumpy Face today. *Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do!</p>
+            <p>I'm nobody's taxi service; I'm not gonna be there to catch you every time you feel like jumping out of a spaceship. I'm the Doctor. Well, they call me the Doctor. I don't know why. I call me the Doctor too. I still don't know why.</p>
+            <p>The way I see it, every life is a pile of good things and bad things.…hey.…the good things don't always soften the bad things; but vice-versa the bad things don't necessarily spoil the good things and make them unimportant. I'm the Doctor. Well, they call me the Doctor. I don't know why. I call me the Doctor too. I still don't know why.</p>
+            <p>*Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do! All I've got to do is pass as an ordinary human being. Simple. What could possibly go wrong? No… It's a thing; it's like a plan, but with more greatness.</p>
+            <p>They're not aliens, they're Earth…liens! No… It's a thing; it's like a plan, but with more greatness. No… It's a thing; it's like a plan, but with more greatness. You hit me with a cricket bat.</p>
+            <p>You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better? It's a fez. I wear a fez now. Fezes are cool. Father Christmas. Santa Claus. Or as I've always known him: Jeff.</p>
+            <p>Annihilate? No. No violence. I won't stand for it. Not now, not ever, do you understand me?! I'm the Doctor, the Oncoming Storm - and you basically meant beat them in a football match, didn't you? You've swallowed a planet!</p>
+            <p>No… It's a thing; it's like a plan, but with more greatness. I'm the Doctor. Well, they call me the Doctor. I don't know why. I call me the Doctor too. I still don't know why. I hate yogurt. It's just stuff with bits in.</p>
+            <p>*Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do! Sorry, checking all the water in this area; there's an escaped fish. All I've got to do is pass as an ordinary human being. Simple. What could possibly go wrong?</p>
+            <p>They're not aliens, they're Earth…liens! I'm the Doctor, I'm worse than everyone's aunt. *catches himself* And that is not how I'm introducing myself. Heh-haa! Super squeaky bum time! No, I'll fix it. I'm good at fixing rot. Call me the Rotmeister. No, I'm the Doctor. Don't call me the Rotmeister.</p>
+            <p>You know when grown-ups tell you 'everything's going to be fine' and you think they're probably lying to make you feel better? Father Christmas. Santa Claus. Or as I've always known him: Jeff. No, I'll fix it. I'm good at fixing rot. Call me the Rotmeister. No, I'm the Doctor. Don't call me the Rotmeister.</p>
+
+
         </div>
       </div>
     );
