bed4c5ca81aab35c5f28e559717fccef03973e30
Sabrina Markon <phpsitescripts@outlook.com>
2019-08-11 00:25:34 -0600

    Added model for skillLevelNames collection.

---
+++ backend/models/skillLevelName.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const skillLevelNameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!value) {
+        new Error('Skill level name is a required field')
+      }
+    }
+  }
+});
+
+module.exports = mongoose.model('skillLevelName', skillLevelNameSchema);
