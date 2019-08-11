757243512c63f8a43c595f86222a1ecbf12f24cb
Sabrina Markon <phpsitescripts@outlook.com>
2019-08-11 00:34:49 -0600

    Team roles model created.

---
+++ backend/models/teamRole.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TeamRoleSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!value) {
+        new Error('User ID is a required field')
+      }
+    }
+  },
+  teamId: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!value) {
+        new Error('Team ID is a required field')
+      }
+    }
+  },
+  jobId: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!value) {
+        new Error('Job ID is a required field')
+      }
+    }
+  }
+});
+
+module.exports = mongoose.model('TeamRole', TeamRoleSchema);
